6c33392ee84895beabe6f879ae2a3ccd25d62ac6
Jonks <anastasiablackhold@gmail.com>
2021-03-30 05:37:32 +0000

    Validations implemented.

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -11,8 +11,8 @@ puts "----------"
 # Your code goes here ...
 
 @store1.employees.create(first_name: "Oscar", last_name: "Astora", hourly_rate: 60)
-@store1.employees.create(first_name: "Siegward", last_name: "Catarina", hourly_rate: 60)
-@store1.employees.create(first_name: "Solaire", last_name: "Astora", hourly_rate: 60)
-@store2.employees.create(first_name: "Orbeck", last_name: "Vinheim", hourly_rate: 60)
-@store2.employees.create(first_name: "Eygon", last_name: "Carim", hourly_rate: 60)
+@store1.employees.create(first_name: "Siegward", last_name: "Catarina", hourly_rate: 20)
+@store1.employees.create(first_name: "Solaire", last_name: "Astora", hourly_rate: 600)
+@store2.employees.create(first_name: "Orbeck", last_name: "Vinheim", hourly_rate: 80)
+@store2.employees.create(first_name: "Eygon", last_name: "Carim", hourly_rate: 70)
 @store2.employees.create(first_name: "Leonard", last_name: "Ringfinger", hourly_rate: 60)
